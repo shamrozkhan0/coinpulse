c19e4c87c0f4b94b9539b8d5a6d125cf9b477262
youngs01 <124503790+youngs01@users.noreply.github.com>
2025-06-28 18:51:12 +0500

    update Readme and Work on the welcome Ui of website to welcome user

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,10 @@
-import { useEffect, useRef } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import Typed from 'typed.js'
 import { motion } from 'framer-motion';
-import 'particles.js'
 import './App.css'
-// import particelJson from ''
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSun, faMoon } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
   const typingInput = useRef(null);
@@ -24,36 +25,54 @@ function App() {
 
   // Load Particles.js
   useEffect(() => {
-
-    if (window.particlesJS && window.particlesJSON) {
-      console.log("particlejs is imported ")
-      window.particlesJS.load('particles-js', '/particles.json', () => {
+    if (window.particlesJS) {
+      window.particlesJS.load('particles-js', 'particles.json', () => {
         console.log("Particles.js loaded");
       });
-
+    } else {
+      console.error("particles.js not available");
     }
-
   }, []);
+
+  const [enabled, setEnabled] = useState(false);
 
   return (
     <>
-      <div id="particles-js" className="w-screen h-screen flex items-center justify-center fixed z-[-1]" >
-        <div className="w-fit relative">
+      <div id="particles-js" className={`flex items-center justify-center absolute inset-0 -z-10 duration-300 ${enabled ? "bg-black" : ''}`}>
+        <div className="w-fit fixed">
           <h1
-            className="text-8xl bg-clip-text text-transparent heading-gradient w-fit transition-all ease-in-out"
+            className="text-6xl sm:text-8xl bg-clip-text text-transparent heading-gradient w-fit transition-all ease-in-out"
             ref={typingInput}
           >
-            &nbsp;
           </h1>
 
-          <motion.p
-            initial={{ opacity: 0, scale: 0 }}
-            animate={{ opacity: 1, scale: 1 }}
-            transition={{ ease: "easeInOut", delay: 0.8, duration: 0.4 }}
-            className="bg-amber-800 text-white font-bold text-2xl px-5 py-2 rounded-4xl absolute left-0 bottom-[-50px]"
-          >
-            Status: Working...
-          </motion.p>
+          <div className="flex items-center justify-between">
+            <motion.p
+              initial={{ opacity: 0, scale: 0 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{ ease: "easeInOut", delay: 0.8, duration: 0.4 }}
+              className="bg-amber-800 text-white font-bold text-sm  sm:text-2xl px-5 py-2 rounded-4xl"
+            >
+              Status: developing
+            </motion.p>
+
+            <motion.div
+              initial={{ opacity: 0, scale: 0 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{ ease: "easeInOut", delay: 0.8, duration: 0.4 }}
+              onClick={() => setEnabled(!enabled)}
+              className={`w-14 h-8 flex items-center rounded-full p-1 cursor-pointer transition-colors duration-300 
+                ${enabled ? 'bg-green-500' : 'bg-gray-400'}`}
+            >
+              <div
+                className={`w-6 h-6 bg-black rounded-full shadow-md transform transition-transform duration-300 
+                              flex items-center justify-center
+                      ${enabled ? 'translate-x-6' : 'translate-x-0'}`}
+              ><FontAwesomeIcon icon={enabled ? faMoon : faSun} className="text-white" />
+              </div>
+            </motion.div>
+
+          </div>
         </div>
       </div>
     </>
